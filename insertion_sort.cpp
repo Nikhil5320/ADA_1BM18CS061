e57fc98c145ed19e87c89b4514dc00ae74ec46c6
Nikhil5320 <53870479+Nikhil5320@users.noreply.github.com>
2020-03-19 20:50:33 +0530

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -2,107 +2,19 @@
 #include <time.h>
 using namespace std;
 
-int insert_sort(int a[],int n)
-{
-	int temp;
-	for(int i=1;i<n;i++)
-	{
-	 for(int j=0;j<i;j++)
-	 {
-	  if(a[i]<a[j])
-	  {#include <iostream>
-#include <time.h>
-using namespace std;
-
-int insert_sort(int a[],int n)
-{
-	int temp;
-	for(int i=1;i<n;i++)
-	{
-	 for(int j=0;j<i;j++)
-	 {
-	  if(a[i]<a[j])
-	  {
-	   temp=a[i];
-	   a[i]=a[j];
-	   a[j]=temp;
- 	  }
-	 }
-	}
-	cout<<"The sorted array by insertion sort is : ";
-        for(int i=0;i<n;i++)
-	 cout<<a[i]<<" ";
-}
-	   
- 	 
-
-int main()
-{
-  clock_t start,end;
-	int a[100],n;
-        cout<<"Enter the number of elements in array"<<endl;
-	cin>>n;
-	cout<<"Enter all "<< n <<" elements of array"<<endl;
-	for(int i=0;i<n;i++)
-	 cin>>a[i];
-        start=clock();
-	insert_sort(a,n);
-        end=clock();
-        cout<<"Time required = "<<(double)(end-start)/CLOCKS_PER_SEC<<endl;
-	return 0;
-}#include <iostream>
-#include <time.h>
-using namespace std;
-
-int insert_sort(int a[],int n)
-{
-	int temp;
-	for(int i=1;i<n;i++)
-	{
-	 for(int j=0;j<i;j++)
-	 {
-	  if(a[i]<a[j])
-	  {
-	   temp=a[i];
-	   a[i]=a[j];
-	   a[j]=temp;
- 	  }
-	 }
-	}
-	cout<<"The sorted array by insertion sort is : ";
-        for(int i=0;i<n;i++)
-	 cout<<a[i]<<" ";
-}
-	   
- 	 
-
-int main()
-{
-  clock_t start,end;
-	int a[100],n;
-        cout<<"Enter the number of elements in array"<<endl;
-	cin>>n;
-	cout<<"Enter all "<< n <<" elements of array"<<endl;
-	for(int i=0;i<n;i++)
-	 cin>>a[i];
-        start=clock();
-	insert_sort(a,n);
-        end=clock();
-        cout<<"Time required = "<<(double)(end-start)/CLOCKS_PER_SEC<<endl;
-	return 0;
-}
-	   temp=a[i];
-	   a[i]=a[j];
-	   a[j]=temp;
- 	  }
-	 }
-	}
-	cout<<"The sorted array by insertion sort is : ";
-        for(int i=0;i<n;i++)
-	 cout<<a[i]<<" ";
-}
-	   
- 	 
+insert_sort(int a[],int n);
+int i, key, j;  
+    for (i = 1; i < n; i++) 
+    {  
+        key = arr[i];  
+        j = i - 1; 
+        while (j >= 0 && arr[j] > key) 
+        {  
+            arr[j + 1] = arr[j];  
+            j = j - 1;  
+        }  
+        arr[j + 1] = key;  
+    }  
 
 int main()
 {
